7e6d851758ecd6972248ae59065260481977dff0
CameronAbo <cabo0@csu.fullerton.edu>
2025-09-21 11:59:36 -0700

    added GPRs backup and restore macro in gpr_backup.inc. Runs with template files

--- supervisor.asm
+++ supervisor.asm
@@ -54,6 +54,7 @@
 
 
 ;Declarations
+%include "gpr_backup.inc"      ;<==This file contains macros that back up and restore the general purpose registers.
 newline equ 10
 null equ 0
 number_of_cells equ 5          ;<==To next open source developer: you may wish to change the size of the array
@@ -80,22 +81,24 @@ segment .text
 manage_arrays:
 
 ;Back up the general purpose registers for the sole purpose of protecting the data of the caller.
-push rbp                                          ;Backup rbp
-mov  rbp,rsp                                      ;The base pointer now points to top of stack
-push rdi                                          ;Backup rdi
-push rsi                                          ;Backup rsi
-push rdx                                          ;Backup rdx
-push rcx                                          ;Backup rcx
-push r8                                           ;Backup r8
-push r9                                           ;Backup r9
-push r10                                          ;Backup r10
-push r11                                          ;Backup r11
-push r12                                          ;Backup r12
-push r13                                          ;Backup r13
-push r14                                          ;Backup r14
-push r15                                          ;Backup r15
-push rbx                                          ;Backup rbx
-pushf                                             ;Backup rflags
+;push rbp                                          ;Backup rbp
+;mov  rbp,rsp                                      ;The base pointer now points to top of stack
+;push rdi                                          ;Backup rdi
+;push rsi                                          ;Backup rsi
+;push rdx                                          ;Backup rdx
+;push rcx                                          ;Backup rcx
+;push r8                                           ;Backup r8
+;push r9                                           ;Backup r9
+;push r10                                          ;Backup r10
+;push r11                                          ;Backup r11
+;push r12                                          ;Backup r12
+;push r13                                          ;Backup r13
+;push r14                                          ;Backup r14
+;push r15                                          ;Backup r15
+;push rbx                                          ;Backup rbx
+;pushf                                             ;Backup rflags
+
+backup    ;<==This macro backs up all general purpose registers.
 
 ;Registers rax, rip, and rsp are usually not backed up.
 
@@ -136,21 +139,23 @@ call showlumber
 ;Prepare to return execution to the caller.
 xorpd xmm0,xmm0                         ;Return 0.0 to the caller
 
-popf                                    ;Restore rflags
-pop rbx                                 ;Restore rbx
-pop r15                                 ;Restore r15
-pop r14                                 ;Restore r14
-pop r13                                 ;Restore r13
-pop r12                                 ;Restore r12
-pop r11                                 ;Restore r11
-pop r10                                 ;Restore r10
-pop r9                                  ;Restore r9
-pop r8                                  ;Restore r8
-pop rcx                                 ;Restore rcx
-pop rdx                                 ;Restore rdx
-pop rsi                                 ;Restore rsi
-pop rdi                                 ;Restore rdi
-pop rbp                                 ;Restore rbp
+;popf                                    ;Restore rflags
+;pop rbx                                 ;Restore rbx
+;pop r15                                 ;Restore r15
+;pop r14                                 ;Restore r14
+;pop r13                                 ;Restore r13
+;pop r12                                 ;Restore r12
+;pop r11                                 ;Restore r11
+;pop r10                                 ;Restore r10
+;pop r9                                  ;Restore r9
+;pop r8                                  ;Restore r8
+;pop rcx                                 ;Restore rcx
+;pop rdx                                 ;Restore rdx
+;pop rsi                                 ;Restore rsi
+;pop rdi                                 ;Restore rdi
+;pop rbp                                 ;Restore rbp
+
+restore    ;<==This macro restores all general purpose registers.
 
 ret                                     ;Pop the integer stack and jump to the address equal to the popped value.
 
